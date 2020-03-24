e6168da1537e65a94c23d6c345ecad416fd3de90
triard <triard78@gmail.com>
2020-03-24 23:21:56 +0700

    11.4 poison debuff

--- Assets/Scripts/Debuffs/PoisonDebuff.cs
+++ Assets/Scripts/Debuffs/PoisonDebuff.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 
 public class PoisonDebuff : Debuff
 {
-    public PoisonDebuff(Monster target) : base(target,1)
+    private float timeSinceTick;
+    
+    private float tickTime;
+
+    private PosionSplash splashPrefab;
+
+    private int splashDamage;
+
+    public PoisonDebuff(int splashDamage, float tickTime,PosionSplash splashPrefab,float duration,Monster target) : base(target,duration)
     {
+        this.splashDamage = splashDamage;
+        this.tickTime = tickTime;
+        this.splashPrefab = splashPrefab;
+    }
+
+    public override void Update()
+    {
+        if (target != null)
+        {
+            timeSinceTick += Time.deltaTime;
+            if (timeSinceTick >= tickTime)
+            {
+                timeSinceTick = 0;
+                Splash();
+            }
+        }
+
+        base.Update();
+    }
+
+    private void Splash()
+    {
+        PosionSplash tmp = GameObject.Instantiate(splashPrefab, target.transform.position, Quaternion.identity);
+
+        tmp.Damage = splashDamage;
+
+        Physics2D.IgnoreCollision(target.GetComponent<Collider2D>(), tmp.GetComponent<Collider2D>());
     }
 }
--- Assets/Scripts/Tower/PoisonTower.cs
+++ Assets/Scripts/Tower/PoisonTower.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class PoisonTower : Tower
 {
+    [SerializeField]
+    private float tickTime;
+    [SerializeField]
+    private PosionSplash splashPrefabs;
+    [SerializeField]
+    private int splashDamage;
+
+    public int SplashDamage
+    {
+        get { return splashDamage; }
+    }
+    public float TickTime
+    {
+        get { return tickTime; }
+    }
+
     private void Start() => ElementType = Element.POISON;
 
     public override Debuff GetDebuff()
     {
-        return new PoisonDebuff(Target);
+        return new PoisonDebuff(splashDamage, tickTime,splashPrefabs,DebuffDuration,Target);    
     }
 }
